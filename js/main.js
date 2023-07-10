11a09fdf1655c0781803810dbaa4a5ee49a7ac89
LorenzoGalimberti <lorenzogalimberti7996@gmail.com>
2023-07-10 11:13:48 +0200

    esercizio completato

--- js/main.js
+++ js/main.js
@@ -24,6 +24,7 @@ Se il codice inserito è valido, dopo aver calcolato il prezzo scontato, elimina
 
 
 let sendButton= document.getElementById("submitButton");
+const promoCodes=["YHDNU32","JANJC63","PWKCN25","SJDPO96","POCIE24"];
 
 sendButton.addEventListener("click", function (event) {
     event.preventDefault(); // Prevent form submission
@@ -31,19 +32,23 @@ sendButton.addEventListener("click", function (event) {
     const backend=20.5;
     const frontend=15.3;
     const project=33.6;
-
-    const promoCodes=["YHDNU32","JANJC63","PWKCN25","SJDPO96","POCIE24"];
+    
+    
     const discount=0.25;
     // 1) ESTRARRE IL TIPO DI LAVORO e le ore richieste e il codice promo
     let tipoLavoro= document.getElementById("inputTypeWork").value;
     let oreRichieste=parseInt(document.getElementById("inputHoursRequested").value);
+    let oreRichiesteContainer=document.getElementById("inputHoursRequested");
     let prezzoFinale=0;
     let promoCode=document.getElementById("inputDiscountCode").value;
-
+    let promoCodeContainer=document.getElementById("inputDiscountCode");
+    
     console.log(promoCode);
     if (oreRichieste < 0) {
         validationMessageHours.innerHTML = "Perfavore inserire un numero positivo .";
+        oreRichiesteContainer.classList.add("errore");
       } else {
+        oreRichiesteContainer.classList.remove("errore");
         validationMessageHours.innerHTML = "";
         if (tipoLavoro=='Backend Development'){
             prezzoFinale=oreRichieste*backend;
@@ -59,9 +64,17 @@ sendButton.addEventListener("click", function (event) {
         if (promoCodes.includes(promoCode)){
             // console.log("funziona");
             prezzoFinale=prezzoFinale*(1-discount);
-        }else{
+            promoCodeContainer.classList.remove("errore");
+            validationMessageDiscount.innerHTML = "";
+            // eliminare l' elemento dall' array
+            let indexOfString=promoCodes.indexOf(promoCode);
+            console.log(indexOfString);
+            delete promoCodes[indexOfString];
+        }else if (promoCode !=""){
             //validationMessageDiscount
             validationMessageDiscount.innerHTML = "Codice sconto non valido .";
+            promoCodeContainer.classList.add("errore");
+            
         }
         // inserimento del prezzo finale nell html
 
