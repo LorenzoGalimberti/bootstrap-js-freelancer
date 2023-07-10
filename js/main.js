fa4d7f9a95c4c4bbb3818af66525a8190c25b2c4
LorenzoGalimberti <lorenzogalimberti7996@gmail.com>
2023-07-10 12:24:29 +0200

    aggiunta funzioni (completed exercise)

--- js/main.js
+++ js/main.js
@@ -1,31 +1,37 @@
-/*
-Continuiamo l’esercizio Bootstrap Freelancer e aggiungiamo la componente js di interazione con l’utente.
-Quando l’utente fa click sul bottone “send” del form, il sito deve calcolare l’ammontare del preventivo per le ore di lavoro richieste.
-Il prezzo orario per una commissione varia in questo modo:
-- se la commissione riguarda lo sviluppo backend il prezzo orario è di 20.5€ l’ora
-- se la commissione riguarda lo sviluppo frontend il prezzo orario è di 15.3€ l’ora
-- se la commissione riguarda l’analisi progettuale di un progetto il prezzo orario è di 33.6€
-L’utente potrebbe decidere di utilizzare un codice promozionale tra i seguenti:
-- YHDNU32
-- JANJC63
-- PWKCN25
-- SJDPO96
-- POCIE24
-Se l’utente inserisce un codice promozionale valido, ha diritto ad uno sconto del 25% sul prezzo finale. Se il codice inserito non è valido, il sito deve informare l’utente che il codice non è valido e il prezzo finale viene calcolato senza applicare sconti.
-Il risultato del calcolo del prezzo finale deve essere visualizzato in “forma umana” (con 2 decimali e il simbolo dell’euro) in un apposito tag HTML appena sotto il bottone send.
-Alcuni consigli
-- Ricordatevi che se non state bene attenti, Javascript vi fa le magie con i tipi :slightly_smiling_face:
-- Ricordatevi che il form ha un comportamento “strano” quando fate click sul bottone Send che è di tipo submit (type=submit).
-Bonus:
-Quando l’utente clicca sul pulsante “Send”, se il codice promozionale inserito non è valido, facciamo diventare quest’ultimo di colore rosso.
-Se il codice inserito è valido, dopo aver calcolato il prezzo scontato, eliminare quel codice dall’elenco dei codici sconto disponibili, in modo che non sia più utilizzabile.
-
-*/
+// ---------------- MAIN -------------------
 
 
 let sendButton= document.getElementById("submitButton");
 const promoCodes=["YHDNU32","JANJC63","PWKCN25","SJDPO96","POCIE24"];
+a=[1,2,3,4]
+console.log(findPosition(a,11));
+//-------------- FUNCTIONS -----------------------
 
+
+// TROVA ELEMENTO
+function trovaElemento(array,elemento){
+    for(let i=0;i<array.length;i++){
+        if(array[i]== elemento){
+            return true;
+        }
+    }
+    return false;
+  }
+
+// FIND POSITION
+
+function findPosition(array,elemento){
+    
+    for(let i=0;i<array.length;i++){
+        if(array[i]==elemento){
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+// ONCLICK FUNCTION
 sendButton.addEventListener("click", function (event) {
     event.preventDefault(); // Prevent form submission
     // VARIABILI
@@ -46,7 +52,9 @@ sendButton.addEventListener("click", function (event) {
     console.log(promoCode);
     if (oreRichieste < 0) {
         validationMessageHours.innerHTML = "Perfavore inserire un numero positivo .";
+        oreRichiesteContainer.classList.remove("errore");
         oreRichiesteContainer.classList.add("errore");
+        
       } else {
         oreRichiesteContainer.classList.remove("errore");
         validationMessageHours.innerHTML = "";
@@ -61,14 +69,16 @@ sendButton.addEventListener("click", function (event) {
             
         }
         // rimaneggiamento del prezzo se presente un codice promo valido 
-        if (promoCodes.includes(promoCode)){
+        //promoCodes.includes(promoCode)
+        if (trovaElemento(promoCodes,promoCode)){
             // console.log("funziona");
             prezzoFinale=prezzoFinale*(1-discount);
             promoCodeContainer.classList.remove("errore");
             validationMessageDiscount.innerHTML = "";
             // eliminare l' elemento dall' array
-            let indexOfString=promoCodes.indexOf(promoCode);
-            console.log(indexOfString);
+            // let indexOfString=promoCodes.indexOf(promoCode);
+            let indexOfString=findPosition(promoCodes,promoCode);
+            //console.log(indexOfString);
             delete promoCodes[indexOfString];
         }else if (promoCode !=""){
             //validationMessageDiscount
@@ -88,3 +98,9 @@ sendButton.addEventListener("click", function (event) {
     
     
   })
+
+
+
+
+  
+  
